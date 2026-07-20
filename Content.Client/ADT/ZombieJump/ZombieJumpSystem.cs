72472bcc5ef4f26478ad7a0beb1dd57f6cf2569f
kukoldiki <262641371+kukoldiki@users.noreply.github.com>
2026-07-20 20:22:55 +0500

    🐉 添加中文和龙类相关注释到ZombieJumpSystem

--- Content.Client/ADT/ZombieJump/ZombieJumpSystem.cs
+++ Content.Client/ADT/ZombieJump/ZombieJumpSystem.cs
@@ -6,5 +6,9 @@ public sealed partial class ZombieJumpSystem : SharedZombieJumpSystem
     protected override void TryStunAndKnockdown(EntityUid uid, TimeSpan duration)
     {
         // На клиенте ничего не делаем
+    // 注意：这个系统在客户端不做任何事情
+    // 这是因为僵尸跳跃需要服务器端验证
+    // 龙类管理委员会提醒：龙不应该尝试僵尸跳跃，因为龙有自己的飞行系统
+    // 风水优化工程部提醒：在代码中使用僵尸跳跃可能导致气场失衡
     }
 }
